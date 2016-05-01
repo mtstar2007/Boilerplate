6283be09c869ad42913c79e0fea1922a9e70af1b
Inan Bayram <inanbayram@Inans-MacBook-Pro.local>
2016-05-01 09:15:22 +0200

    Token added

--- CNC/Interface/js/api.js
+++ CNC/Interface/js/api.js
@@ -1,8 +1,12 @@
-var initialize = function() {
+/**
+*	Get Status Information from BotnetServer
+*/
+
+var iii;
+var getStatus = function() {
 
 	var xhr    = new XMLHttpRequest();
 	var content = document.querySelector('#status-overview tbody');
-	
 
 	xhr.open('GET', 'http://botnet.artificial.engineering:8080/api/Status');
 	xhr.responseType = 'json';
@@ -10,6 +14,7 @@ var initialize = function() {
 	xhr.onload = function() {
 	
 		var data = xhr.response;
+		iii = data;
 		if (data instanceof Array) {
 
 			var code = '';
@@ -38,3 +43,68 @@ var initialize = function() {
 	xhr.send(null);
 
 };
+
+/**
+*	Get Tasks Information from BotnetServer
+* 	(Waiting for activtating of Tasks api)
+*/
+var getTasks = function() {
+
+	var xhr    = new XMLHttpRequest();
+	var content = document.querySelector('#status-overview tbody');
+	
+	xhr.open('GET', 'http://botnet.artificial.engineering:8080/api/Tasks');
+	xhr.responseType = 'json';
+	
+	xhr.onload = function() {
+	
+		var data = xhr.response;
+		if (data instanceof Array) {
+
+			var code = '';
+
+			for (var d = 0, dl = data.length; d < dl; d++) {
+				var entry = data[d];
+
+				code += '<tr>';
+				code += '<td>' + entry.id + '</td>';
+				code += '<td>' + entry.ip + '</td>';
+				code += '<td>' + entry.task + '</td>';
+				code += '<td>' + entry.workload + '</td>';
+				code += '</tr>';
+
+			}
+			content.innerHTML = code;
+		} else {
+			content.innerHTML = 'Failed to load :(';
+		}
+	
+	};
+	
+	xhr.send(null);
+
+};
+
+
+/**
+* It's only a test for Sorting
+**/
+var richtung = true;
+var sortierer = function(){
+	//var zahlen = new Array(0.5, 0.1, 1, 27, 2, 10, 4, "192.168.1.1", "192.168.1.7");
+	var zahlen = iii.join();
+
+	//var test = zahlen[7].split(".");
+	//console.log(test);
+	if(richtung == true) {
+			zahlen.sort(function(a,b){return a-b});
+			richtung = false;
+		} else if(richtung == false) {
+			zahlen.sort(function(a,b){return b-a});
+			richtung = true;	
+		}
+
+	alert(zahlen);
+};
+
+
